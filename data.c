28d705b9ad552015c7db467e823f2bc4bc2cc0d0
Krzysztof Szymański <kszymanski926@gmail.com>
2026-04-21 00:12:27 +0200

    shouldSend

--- data.c
+++ data.c
@@ -50,7 +50,7 @@ uint32_t addrGetBroadcast (NetAddr na) {
 }
 
 bool shouldSend(NetData *nd, int turn) {
-    return !nd->direct || nd->direct && (turn - nd->last_seen < TURNS_AFTER_INF);
+    return !nd->direct || nd->active_network || (turn - nd->last_seen < TURNS_AFTER_INF);
 }
 
 void propagateInfinity (NetData *nd, RoutingTable rt, int turn) {
